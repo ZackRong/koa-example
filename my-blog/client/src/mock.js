7ee5871dbd87f15009e1b3b157d6a6f3eb5e3af7
rongzhenxin <zx.rong@zuolin.com>
2020-08-24 23:49:02 +0800

    mock

--- my-blog/client/src/mock.js
+++ my-blog/client/src/mock.js
@@ -20,10 +20,10 @@ Mock.setup({ timeout: '100-600' });
 //     }
 //   ]
 // });
-let List = [];
+let List = [], count = 40;
 const blogMockTemplate = {
   // 'token': Random.guid(),
-  'createTime': Mock.Random.datetime(),
+  // 'createTime': Mock.Random.datetime(),
   'title': '@ctitle',
   'content': '@cparagraph',
   'creator': '@name',
@@ -32,21 +32,44 @@ const blogMockTemplate = {
   'agreeCount|+1': Random.natural(0, 100)
 };
 
-Mock.mock('/api/listBlog', function (options) {
+for (let i = 0; i < count; i++) {
+  List.push(Mock.mock({
+    'token': Random.guid(),
+    'createTime': Mock.Random.datetime(),
+    ...blogMockTemplate
+  }));
+}
+
+Mock.mock('/api/listBlog', (options) => {
   let { body } = options || {};
+  let result = [];
   if (body) {
     body = JSON.parse(body);
-    const { count = 0 } = body;
-    console.log(count)
-    for (let i = 0; i < count; i++) {
-      List.push(Mock.mock({
-        'token': Random.guid(),
-        ...blogMockTemplate
-      }));
+    const { pageSize = 10, page = 1 } = body;
+    const totalPage = Math.ceil(List.length / pageSize);
+    if (page <= totalPage) {
+      result = List.slice(pageSize * (page - 1), pageSize * page);
     }
   }
   return {
     errorCode: 200,
-    response: List
+    response: {
+      list: result,
+      total: List.length
+    }
   };
 });
+
+Mock.mock('/api/deleteBlog', (options) => {
+  let { body } = options || {};
+  if (body) {
+    body = JSON.parse(body);
+    const { token } = body;
+    List = List.filter(item => item.token !== token);
+    return {
+      errorCode: 200,
+      errorMessage: 'success'
+    };
+  }
+});
+
--- my-blog/client/src/routes/Home/routes/List/index.js
+++ my-blog/client/src/routes/Home/routes/List/index.js
@@ -1,5 +1,5 @@
 import React, { Component, PureComponent, Fragment } from 'react';
-import { Spin } from 'antd';
+import { Spin, Button, Modal, Pagination } from 'antd';
 import { LikeOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import moment from 'moment';
@@ -12,29 +12,69 @@ class List extends (PureComponent || Component) {
   state = {
     loading: true,
     blogList: [],
+    total: 0,
+    currentPage: 1,
+    pageSize: 10,
   }
 
   componentDidMount() {
+    this.list();
+  }
+
+  list = () => {
+    this.setState({loading: true});
+    const { pageSize, currentPage } = this.state;
     axios({
       method: 'post',
       url: '/api/listBlog',
       data: {
-        count: 10
+        pageSize,
+        page: currentPage
       }
     }).then((res = {}) => {
       const { data = {} } = res;
-      const { errorCode, response = [] } = data;
+      const { errorCode, response = {} } = data;
       console.log(response)
       if (errorCode === 200) {
-        this.setState({blogList: response});
+        const { list = [], total } = response;
+        this.setState({
+          blogList: list,
+          total
+        });
       }
     }).finally(() => {
       this.setState({loading: false});
     })
   }
 
+  // 删除
+  onDelete = (token) => {
+    Modal.confirm({
+      title: '确定删除吗？',
+      content: '此操作不可逆转，删除后数据将永久消失',
+      onOk: () => {
+        axios({
+          method: 'post',
+          url: '/api/deleteBlog',
+          data: {token}
+        }).then(() => {
+          this.list();
+        });
+      }
+    })
+  }
+
+  // 页码/每页大小修改
+  onChange = (currentPage, newPageSize) => {
+    const { pageSize } = this.state;
+    this.setState({
+      currentPage: +pageSize === +newPageSize ? currentPage : 1,
+      pageSize: newPageSize
+    }, this.list);
+  }
+
   render () {
-    const { loading, blogList } = this.state;
+    const { loading, blogList, currentPage, total } = this.state;
     return (
       <Spin spinning={loading}>
         <div className={prefixCls}>
@@ -70,11 +110,24 @@ class List extends (PureComponent || Component) {
                               {`(${agreeCount})`}
                             </div>
                           )}
+                          <Button
+                            className='delete'
+                            onClick={() => this.onDelete(token)}
+                          >
+                            删除
+                          </Button>
                         </div>
                       </a>
                     );
                   })
                 }
+                <Pagination
+                  current={currentPage}
+                  total={total}
+                  onChange={this.onChange}
+                  pageSizeOptions={['10', '20']}
+                  showSizeChanger
+                />
               </Fragment>
             )
           }
